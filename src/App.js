fc4bd7177427a91321f17f5def081514012e806d
undertherose <32499234+undertherose@users.noreply.github.com>
2023-05-10 13:49:50 -0400

    test

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
       <div>
         <h1>The path to your next</h1>
         <h2>Open Source</h2>
-        <h1>contribution.</h1>
+        <h1>contribution.2</h1>
       </div>
     </div>
   );
