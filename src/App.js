a76d8572d89d110ded47eabd1a073d42ce89e5e3
undertherose <32499234+undertherose@users.noreply.github.com>
2023-05-10 13:44:53 -0400

    update

--- src/App.js
+++ src/App.js
@@ -23,6 +23,11 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <h1>The path to your next</h1>
+        <h2>Open Source</h2>
+        <h1>contribution.</h1>
+      </div>
     </div>
   );
 }
